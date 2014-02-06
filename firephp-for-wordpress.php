558eff8e2c0b034b4c58eb94b66fcc056f6dcc65
Jessica Oros <orosjess@gmai.com>
2014-02-06 23:20:34 +0000

    First Commit

---
+++ firephp-for-wordpress.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: FirePHP for WordPress
+ * Description: Integrates FirePHP functionality with WordPress
+ * Version: .1
+ * Author: Jessica Oros
+ */
+
+require_once(dirname(__FILE__)) . '/lib/FirePHPCore/FirePHP.class.php';
+require_once(dirname(__FILE__)) . '/lib/FirePHPCore/fb.php';
+
+ob_start();
